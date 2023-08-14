e620e1fe52b754ce71c593fddd3280f493a8baab
Deyvid Santos <deyvidsantosdatascience@gmail.com>
2023-08-14 10:21:17 -0300

    refact: V1 Controller

--- src/main/java/deyvid/silva/scholarship/repository/ClassRepository.java
+++ src/main/java/deyvid/silva/scholarship/repository/ClassRepository.java
@@ -4,6 +4,11 @@ import deyvid.silva.scholarship.entity.Class;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ClassRepository extends JpaRepository<Class, Integer> {
+    List<Class> findByCoordinator_Id(Integer coordinatorId);
+    List<Class> findByInstructors_Id(Integer instructorId);
+    List<Class> findByScrumMaster_Id(Integer scrumMasterId);
 }
